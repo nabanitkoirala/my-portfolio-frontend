582435c2dcbd01de3be3cadcc75f489ff343e614
nabanit <nabanitkoirala@gmail.com>
2023-08-17 09:53:55 +0545

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 
-import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Navigate, RouterProvider, createBrowserRouter, useLocation } from 'react-router-dom';
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 import Qualifications from "./components/Qualifications";
 import Dashboard from './Dashboard';
 import Login from './admin/pages/LoginPage';
 import Homepage from './admin/pages/HomePage';
+import ReactGA from 'react-ga';
+import { useEffect } from 'react';
+
+const TRACKING_ID = import.meta.env.VITE_APP_GOOGLE_ANALYTICS_TRACKING_ID;
 const App = () => {
+
+  const { pathname, search } = useLocation();
+  ReactGA.initialize(TRACKING_ID);
   const router = createBrowserRouter([
     // {
     //   path: '/',
@@ -88,6 +95,10 @@ const App = () => {
     // },
     { path: '*', element: <Navigate to="/" /> },
   ]);
+  useEffect(() => {
+    ReactGA.set({ page: pathname, title: 'Homepage' });
+    ReactGA.pageview(pathname + search);
+  }, [pathname, search]);
   return (
     <RouterProvider router={router} />
   );
