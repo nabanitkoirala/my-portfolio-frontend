22c00a1ae09b458e520c8075c4b3d8ce55bf9c44
nabanit <nabanitkoirala@gmail.com>
2023-08-17 10:32:52 +0545

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,38 @@
 
-import { Navigate, RouterProvider, createBrowserRouter, useLocation } from 'react-router-dom';
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
-import Qualifications from "./components/Qualifications";
+import { BrowserRouter, Navigate, useLocation, Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
-import Login from './admin/pages/LoginPage';
-import Homepage from './admin/pages/HomePage';
 import ReactGA from 'react-ga';
 import { useEffect } from 'react';
 
 const TRACKING_ID = import.meta.env.VITE_APP_GOOGLE_ANALYTICS_TRACKING_ID;
-const App = () => {
-
+const MyComponent = () => {
   const { pathname, search } = useLocation();
   ReactGA.initialize(TRACKING_ID);
-  const router = createBrowserRouter([
-    // {
-    //   path: '/',
-    //   element: (
-    //     <Layout
-    //       handlePrompt={handlePrompt}
-    //       deferredPrompt={deferredPrompt}
-    //     >
-    //       <Dashboard
 
-    //       />
-    //     </Layout>
-    //   ),
-    // },
-    // {
-    //   path: '/',
-    //   element: (
-    //     <Login />
-    //   ),
-    // },
-    {
-      path: '/',
-      children: [
-        {
-          index: true,
-          path: '',
-          element: (
-
-            <Dashboard />
-
-          ),
-        },
-
-      ],
-    },
-    {
-      path: '/login',
-      children: [
-        {
-          index: true,
-          path: '',
-          element: (
-
-            <Login />
-
-          ),
-        },
-
-      ],
-    },
-    {
-      path: '/dashboard',
-      children: [
-        {
-          index: true,
-          path: '',
-          element: (
-
-            <Homepage />
-
-          ),
-        },
-
-      ],
-    },
-    // {
-    //   path: '/all-students',
-    //   children: [
-    //     {
-    //       index: true,
-    //       path: '',
-    //       element: (
-    //         <Layout>
-    //           <AllStudents />
-    //         </Layout>
-    //       ),
-    //     },
-
-    //   ],
-    // },
-    { path: '*', element: <Navigate to="/" /> },
-  ]);
   useEffect(() => {
     ReactGA.set({ page: pathname, title: 'Homepage' });
     ReactGA.pageview(pathname + search);
   }, [pathname, search]);
   return (
-    <RouterProvider router={router} />
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Dashboard />
+
+        }
+      />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+}
+const App = () => {
+
+
+  return (
+    <BrowserRouter>
+      <MyComponent />
+    </BrowserRouter>
   );
 }
 
--- src/admin/components/SVG.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SVG = ({ children }) => {
-  return <>{children}</>;
-};
-
-export default SVG;
